65546e682b4d1a3c6a64998d298c6ae3559d828d
ChristopherL <101825137+ChristopherL-creator@users.noreply.github.com>
2022-05-11 23:28:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ const bookTemplate = `
     <div class="copies border"> 
         #COPIES 
     </div>
-    <button id='buy-button' class="button border">Acquista</button>
+  
     <div class="author-editor-year border"> 
     
     </div>
@@ -72,8 +72,7 @@ function displayBookWithTemplate(template, container, array) {
 
     for (let i = 0; i < array.length; i++) {
         const book = array[i];
-
-        const div = document.createElement('div');
+        const div = document.createElement('div'); 
 
         const bookTemplate = template.replace('#TITLE', book.title)
                                      .replace('#TYPE', 'Type: ' + book.type)
@@ -82,31 +81,42 @@ function displayBookWithTemplate(template, container, array) {
 
         div.innerHTML = bookTemplate;
         mainContainer.appendChild(div);  
+        const bookContainer = div.querySelector('.book-container');
+        const buyButton = document.createElement('button'); 
+        buyButton.appendChild(document.createTextNode('Acquista'));  
+        buyButton.classList.add('button');
+        buyButton.addEventListener('click', () => sellBook(book)); 
+        // buyButton.addEventListener('click', () => removeFinishedBooks(book)); 
+                //  trasformo metodo in lambda, così posso passare tutti iparametri che voglio;
+        bookContainer.appendChild(buyButton);
+        
 
+        
+        
         const infoContainer = div.querySelector('.author-editor-year');
         const authorDiv = document.createElement('div');
         const author = document.createTextNode(book.author);
-        authorDiv.classList.add('info-item')
+        authorDiv.classList.add('info-item');
         authorDiv.appendChild(author);
         infoContainer.appendChild(authorDiv);
 
         const editorDiv = document.createElement('div');
         const editor = document.createTextNode(book.editor);
-        editorDiv.classList.add('info-item')
+        editorDiv.classList.add('info-item');
         editorDiv.appendChild(editor);
         infoContainer.appendChild(editorDiv);
-
         
         const yopDiv = document.createElement('div');
         const yop = document.createTextNode(book.yop);
-        yopDiv.classList.add('info-item')
+        yopDiv.classList.add('info-item');
         yopDiv.appendChild(yop);
-        infoContainer.appendChild(yopDiv);
+        infoContainer.appendChild(yopDiv); 
+        bookContainer.appendChild(infoContainer);
 
         const priceContainer = div.querySelector(".price-discount");
         const priceDiv = document.createElement('div');
         const price = document.createTextNode(book.getPubblicPrice() + '€');
-        priceDiv.classList.add('info-item')
+        priceDiv.classList.add('info-item');
         priceDiv.appendChild(price);
         priceContainer.appendChild(priceDiv); 
 
@@ -115,18 +125,34 @@ function displayBookWithTemplate(template, container, array) {
         discountDiv.classList.add('price-item')
         discountDiv.appendChild(discount);
         priceContainer.appendChild(discount);
-
+        bookContainer.appendChild(priceContainer);
+        mainContainer.appendChild(bookContainer); 
 
     }
 }
 
 displayBookWithTemplate(bookTemplate, 'book-list-container', bookArray) 
 
-const buyButton = document.getElementById('buy-button'); 
-   
-buyButton.addEventListener('click', function(){
-            bookArray.copies = parseInt(bookArray.copies) + 1; // `parseInt` converts the `value` from a string to a number
-          }, false); 
+function sellBook(book) {         
+    
+    book.copies = book.copies - 1; 
+    if (book.copies === 0) { 
+//  creo nuovo array, in cui inserisco book con copie superiori a 0;
+        const tempArray = []; 
+        for (let i = 0; i < bookArray.length; i++) {
+            const b = bookArray[i];
+            if (b.copies > 0) {
+                tempArray.push(b);
+            }
+        } 
+        bookArray = tempArray;
+    } 
+
+    
+    displayBookWithTemplate(bookTemplate, 'book-list-container', bookArray); 
+//  citando funzione, forzo riscrittura;
+} 
+
 
 function removeFinishedBooks(book){
     bookArray = bookArray.filter(b => b.copies === 0); 
@@ -135,57 +161,4 @@ function removeFinishedBooks(book){
     displayBookWithTemplate(finishedTemplate, 'finished-container', finishedArray);
 } 
  
-// const libro1 = {title: 'odissea', copies: 5}; 
-// const libro2 = {title: 'iliade', copies: 15}; 
-// const libro3 = {title: 'i promessi sposi', copies: 3}; 
-
-// let bookList = [libro1, libro2, libro3]; 
-
-// function displayBooks() { 
-
-//     const template = ` 
-//         <span><strong> #TITLE </strong> </span> <br> 
-//         <span> copie: #COPIES </span>      
-//     ` 
-    
-//     const bookListContainer = document.getElementById('book-list-container'); 
-
-//     bookListContainer.innerHTML = '';
-
-//     for (const book of bookList) {
-        
-//         const bookContainer = document.createElement('div'); 
-
-//         const replacedTemplate = template.replace('#TITLE', book.title).replace('#COPIES', book.copies); 
-
-//         bookContainer.innerHTML = replacedTemplate; 
-
-//         const button = document.createElement('button'); 
-//         button.appendChild(document.createTextNode('vendi'));  
-//         button.addEventListener('click', () => sellBook(book)); 
-//         //  trasformo metodo in lambda, così posso passare tutti iparametri che voglio;
-//         bookContainer.appendChild(button);
-
-//         bookListContainer.appendChild(bookContainer)
-//     }
-// } 
-
-// displayBooks(); 
-
-// function sellBook(book) {
-//     book.copies = book.copies - 1; 
-//     if (book.copies === 0) { 
-// //  creo nuovo array, in cui inserisco book con copie superiori a 0;
-//         const tempArray = []; 
-//         for (let i = 0; i < bookList.length; i++) {
-//             const b = bookList[i];
-//             if (b.copies > 0) {
-//                 tempArray.push(b);
-//             }
-//         } 
-//         bookList = tempArray;
-//     }
-//     displayBooks(); 
-// //  citando funzione, forzo riscrittura;
-// } 
 
